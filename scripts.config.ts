f76c995392fbddaff702a9eb63033197fd72f946
pmagaz <magazpablo@gmail.com>
2020-11-21 12:11:20 +0100

    Add denon config

---
+++ scripts.config.ts
@@ -0,0 +1,14 @@
+import { DenonConfig } from "https://deno.land/x/denon@2.4.4/mod.ts";
+
+const config: DenonConfig = {
+  watch: true,
+  scripts: {
+    start: {
+      cmd: "deno run --allow-net --allow-read --allow-env src/server.ts",
+      desc: "Run server",
+      watch: true,
+    },
+  },
+};
+
+export default config;
