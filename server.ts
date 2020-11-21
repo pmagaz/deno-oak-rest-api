c4e5dd7700b6d61c5a0a12e905a75d87ace4e943
pmagaz <magazpablo@gmail.com>
2020-11-21 10:43:06 +0100

    Add .env config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Context, Router } from "https://deno.land/x/oak/mod.ts";
 
-const port = 8000;
+const { PORT } = config();
 const router = new Router();
 const app = new Application();
 app.use(router.routes());
@@ -10,6 +11,6 @@ router.get("/", (context: Context) => {
   context.response.body = "Hello world!";
 });
 
-console.log(`Server up on port ${port}`);
+console.log(`Server up on port ${PORT}`);
 
-await app.listen({ port });
+await app.listen({ port: Number(PORT) });
