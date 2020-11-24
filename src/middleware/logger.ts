994a65206b30453200e93433ccd7dfb791586a79
pmagaz <magazpablo@gmail.com>
2020-11-24 18:41:26 +0100

    Update middleware

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,8 @@
 import type { Context } from "https://deno.land/x/oak/mod.ts";
 
 export const logger = async (ctx: Context, next: () => any) => {
+  await next();
   const body = await ctx.request.body().value;
   let params = body ? `with params ${JSON.stringify(body)}` : "";
   console.log(`${ctx.request.method} request to ${ctx.request.url} ${params}`);
-  await next();
 };
