e13388cbb52f33d1f4f98be6d1895aac6cbd7525
pmagaz <magazpablo@gmail.com>
2020-11-24 18:46:00 +0100

    Rename file

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,12 +1,11 @@
 import { Context, helpers } from "https://deno.land/x/oak/mod.ts";
 import type { User } from "../types/user.ts";
-import * as db from "../db/user.ts";
+import * as db from "../db/index.ts";
 
 export const findUser = async (ctx: Context) => {
   const { userId } = helpers.getQuery(ctx, { mergeParams: true });
   try {
     let user: User = await db.findUserById(userId);
-    ctx.response.status = 200;
     ctx.response.body = user;
   } catch (err) {
     ctx.response.status = 404;
@@ -18,7 +17,6 @@ export const createUser = async (ctx: Context) => {
   const { name, birthDate } = await ctx.request.body().value;
   try {
     let createdUser: User = await db.createUser(name, birthDate);
-    ctx.response.status = 200;
     ctx.response.body = createdUser;
   } catch (err) {
     ctx.response.status = 500;
@@ -27,11 +25,9 @@ export const createUser = async (ctx: Context) => {
 };
 
 export const updateUser = async (ctx: Context) => {
-  ctx.response.status = 200;
   ctx.response.body = { msg: "User updated!" };
 };
 
 export const deleteUser = async (ctx: Context) => {
-  ctx.response.status = 200;
   ctx.response.body = { msg: "User deleted!" };
 };
